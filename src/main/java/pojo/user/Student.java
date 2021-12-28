3afffb7fe213c91ac2b904c03ce41034d51ddb6a
pluto <2391783880@qq.com>
2021-12-28 14:17:50 +0800

    The first Mybatis program  2021.12.27

--- src/main/java/pojo/user/Student.java
+++ src/main/java/pojo/user/Student.java
@@ -1,5 +1,8 @@
 package pojo.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.ibatis.type.Alias;
 
 import java.util.Date;
@@ -9,80 +12,14 @@ import java.util.Date;
  * @version 1.0
  */
 @Alias("student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender=" + gender +
-                ", subject='" + subject + '\'' +
-                ", grade=" + grade +
-                ", add_time='" + add_time + '\'' +
-                '}';
-    }
-
     private int id;
     private String name;
     private int gender;
     private String subject;
     private double grade;
     private Date add_time;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGender() {
-        return gender;
-    }
-
-    public void setGender(int gender) {
-        this.gender = gender;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public double getGrade() {
-        return grade;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
-
-    public Date getAdd_time() {
-        return add_time;
-    }
-
-    public void setAdd_time(Date add_time) {
-        this.add_time = add_time;
-    }
-
-    public Student(int id, String name, int gender, String subject, double grade, Date add_time) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.subject = subject;
-        this.grade = grade;
-        this.add_time = add_time;
-    }
 }
--- src/main/java/pojo/user/User.java
+++ src/main/java/pojo/user/User.java
@@ -1,70 +1,22 @@
 package pojo.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 /**
  * @author Hoaer
  * @version 1.0
  */
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private int id;
     private String username;
     private String password;
     private String phone;
-    private final Date registration_datetime;
-
-    public User(int id, String username, String password, String phone, Date registration_datetime) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.phone = phone;
-        this.registration_datetime = registration_datetime;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Date getRegistration_datetime() {
-        return registration_datetime;
-    }
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", phone='" + phone + '\'' +
-                ", registration_datetime='" + registration_datetime + '\'' +
-                '}';
-    }
+    private Date registration_datetime;
 }
